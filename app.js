9ade4a9b4114b9ca1fb41d1205badf5877c15422
BilalAli666 <119819487+BilalAli666@users.noreply.github.com>
2023-02-06 21:08:35 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,339 @@
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ARRAYS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ARRAYS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ARRAYS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// Q:1
+
+// const arr = ['apple' ,'kiwi' ,'mango' ,'banana']
+
+// Q:2
+
+// const arr = [1,2,3,1,2,3,4,4,5,56,6,12]
+
+// Q:3
+
+// const arr = [true,false,true,false,true,false,true,false,true,false,]
+
+// Q:4
+
+// const arr = [1,23,4,4,5,5,4,3,3,"apple",true]
+
+// Q:5
+
+// const arr = ['ufone','telenore','jazz','zong','warid']
+
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ for loops ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ for loops ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ for loops ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// Q:3
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~table~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+// var num1 = prompt('type the nubmer of what you want table')
+// var num2 = prompt("type the number")
+
+// for(var i=1;i<=num2;i++){
+//     document.write(num1 + " x " + i + " = " + num1*i + "<br>")
+// }
+
+// var i = 1
+// while(i<=num2){
+//     document.write(num1 + " x " + i + " = " + num1*i + "<br>")
+//     i++
+// }
+
+// var i = 1
+// do{
+//     document.write(num1 + " x " + i + " = " + num1*i + "<br>");
+//     i++
+// }
+// while(i<=num2)
+
+
+// Q:1
+
+// for(var i=1;i<=10;i++){
+//     document.write(i+"<br>")
+// }
+
+
+// Q:4
+
+
+// const arr = ["Nokia", "Samsung", "Apple", "Sony", "Huawei"]
+
+// for(var i=1;i<=5;i++){
+//     document.write(arr.shift(i)+"<br>")
+// }
+
+
+// Q:5
+
+
+// const arr = ["apple", "banana", "mango", "orange", "strawberry"]
+
+// for(var i=1;i<=5;i++){
+//     document.write("ELEMENT INDEX AT    " + i +" " + arr.shift(i)+"<br>" )
+// }
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ IFStatements ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ IFStatements ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ IFStatements ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// Q:1
+
+
+// var num1 = prompt("enter number you want to check that the is divisible by 3")
+
+
+// if (num1%3 == 0){
+//     document.write("YES! IT IS")
+// }
+// else{
+//     document.write("NO IT,S NOT")
+// }
+
+// Q:2
+
+// var num1 = prompt("enter number you want to check that the is divisible by 3")
+
+
+// if (num1%2 == 0){
+//     document.write("YES! IT IS")
+// }
+// else{
+//     document.write("NO IT,S NOT")
+// }
+
+// Q:3
+
+// var num1 = prompt("enter number you want to check that the is divisible by 3")
+
+
+// if (num1<18){
+//     document.write("TOO YOUNG")
+// }
+// else{
+//     document.write("OLD ENOUGH")
+// }
+
+// Q:4
+
+// var num1 = prompt("your name")
+
+// num1 = num1.toLowerCase()
+// document.write(num1 + "<br>")
+// if (num1 == 'bilal'){
+//     document.write("HELLO BILAL SIR")
+// }
+// else{
+//     document.write("you are not suppos to be hear it's private")
+// }
+
+
+// var num1 = prompt('ENTER FIRST NUMBER')
+// var num2 = prompt('ENTER SECOND NUMBER')
+
+// if(num1 == num2 ){
+//     document.write ('both are equal')
+// }
+// else if(num1 < num2 ){
+//     document.write (num1 +" is less then "+ num2)
+// }
+// else if(num1 > num2 ){
+//     document.write (num1 +" is greater then "+ num2)
+// }
+
+
+
+
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ allerts ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ allerts ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ allerts ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+// Q:1
+
+
+// alert("WELCOME TO MY WEBSITE")
+
+// Q:2
+
+// alert("ERROR! PLEASE ENTER VALID PASSWORD")
+
+// Q:3
+
+// alert("WELCOME TO JS LAND .... \n HAPPY CODING")
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ STRINGS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ STRINGS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ STRINGS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// var fname=prompt('enter your first name')
+// var lname=prompt('enter your first name')
+
+// alert("welcome" )
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~AIR FOR SHARE TASKS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~AIR FOR SHARE TASKS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~AIR FOR SHARE TASKS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~        Q1        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// var num=prompt("please enter a number to check taht it is true or false")
+
+// if (num%2 == 0){
+//     document.write("even")
+// }
+// else{
+//     document.write("odd")
+// }
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~        Q2        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// var num = prompt("enter value to check")
+
+// if(number)
+
+
+
+
+
+
+
+
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~        Q3        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+// var num1 = prompt('ENTER FIRST NUMBER')
+// var num2 = prompt('ENTER SECOND NUMBER')
+
+// if(num1 == num2 ){
+//     document.write ('both are equal')
+// }
+// else if(num1 < num2 ){
+//     document.write (num1 +" is less then "+ num2)
+// }
+// else if(num1 > num2 ){
+//     document.write (num1 +" is greater then "+ num2)
+// }
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~        Q4        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+// var num1 = prompt('ENTER FIRST NUMBER')
+// var num2 = prompt('ENTER SECOND NUMBER')
+// var num3 = prompt('ENTER THIRED NUMBER')
+
+// if(num1 < num2 && num3 < num2 ){
+//     document.write (num2 +" is greatest ")
+// }
+// else if(num1 < num3 && num1 < num3 ){
+//     document.write (num3 +" is greatest ")
+// }
+
+
+// else if(num2 < num1 && num3 < num1 ){
+//     document.write (num1 +" is greatest ")
+// }
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~        Q5        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// var num1 = prompt('ENTER FIRST NUMBER')
+// var num2 = prompt('ENTER SECOND NUMBER')
+// var num3 = prompt('ENTER THIRED NUMBER')
+
+// if(num1 == num2 && num3 == num2 ){
+//     document.write ("Equilateral triangle")
+// }
+// else if(num1 == num3 || num2 == num3 || num1 == num2 ){
+//     document.write ("Isosceles triangle")
+// }
+
+
+// else {
+//     document.write ("Scalene triangle")
+// }
+
+
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~        Q6        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~        Q7        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~        Q8        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~        Q9        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~        Q10       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
